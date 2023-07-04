426866eb6f8b8238d93e0bd4618b04e3caa8be4e
ThomasBTR <64105226+ThomasBTR@users.noreply.github.com>
2023-07-04 12:02:46 +0200

    update article view with personnalize feed per theme subscribed

--- front/src/app/features/articles/components/list/list.component.ts
+++ front/src/app/features/articles/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SessionInformation} from "../../../../interfaces/sessionInformation.interface";
 import {SessionService} from "../../../../services/session.service";
 import {map, Observable} from "rxjs";
@@ -6,20 +6,25 @@ import {Article} from "../../interfaces/article.interface";
 import {ArticlesApiService} from "../../services/articles-api.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {OrderBy} from "../../interfaces/OrderByList.interface";
+import {UserService} from "../../../../services/user.service";
+import {User} from "../../../../interfaces/user.interface";
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent {
+export class ListComponent implements OnInit {
 
   public articles$: Observable<Article[]> = this.sessionApiService.getAllArticles();
   public orderByList: OrderBy[] | undefined;
+  private userInfo: User | undefined;
+  private themeList: String[] | undefined;
 
   constructor(
     private sessionService: SessionService,
     private sessionApiService: ArticlesApiService,
+    private userService: UserService,
     private route: ActivatedRoute,
     private router: Router
   ) {
@@ -35,6 +40,23 @@ export class ListComponent {
     ];
   }
 
+  ngOnInit() {
+    this.userService.getById(this.sessionService.sessionInformation!.id.toString()).subscribe((user) => {
+      this.themeList = user.subscriptions?.map((subscription) => {
+        return subscription.title;
+      });
+      if (this.themeList) {
+        this.articles$ = this.articles$.pipe(
+          map((articles) => {
+            return articles.filter((article) => {
+              return this.themeList?.includes(article.theme);
+            });
+          }));
+      }
+      ;
+    });
+  }
+
   get user(): SessionInformation | undefined {
     return this.sessionService.sessionInformation;
   }
@@ -44,36 +66,52 @@ export class ListComponent {
   }
 
   orderArticlesBy(orderBy: string) {
-    if (orderBy === 'Thèmes') {
-      this.articles$.subscribe((articles) => {
-        this.articles$ = this.articles$.pipe(
-          map((articles) => {
-            articles.sort((a, b) => {
-              return a.theme > b.theme ? 1 : -1;
-            });
-            return articles;
-          }));
-      });
-    } else if (orderBy === 'Date') {
-      this.articles$.subscribe((articles) => {
-        this.articles$ = this.articles$.pipe(
-          map((articles) => {
-            articles.sort((a, b) => {
-              return a.date > b.date ? 1 : -1;
-            });
-            return articles;
-          }));
-      });
-    } else if (orderBy === 'Auteur') {
-      this.articles$.subscribe((articles) => {
-        this.articles$ = this.articles$.pipe(
-          map((articles) => {
-            articles.sort((a, b) => {
-              return a.author > b.author ? 1 : -1;
-            });
-            return articles;
-          }));
-      });
+    switch (orderBy) {
+      case 'Thèmes':
+        this.orderByThemes(this.orderByList);
+        break;
+      case 'Date':
+        this.orderByDate(this.orderByList);
+        break;
+      case 'Auteur':
+        this.orderByAuthor(this.orderByList);
+        break;
     }
+  }
+
+  private orderByAuthor(orderByList: OrderBy[] | undefined) {
+    this.articles$.subscribe((articles) => {
+      this.articles$ = this.articles$.pipe(
+        map((articles) => {
+          articles.sort((a, b) => {
+            return a.author > b.author ? 1 : -1;
+          });
+          return articles;
+        }));
+    });
+  }
+
+  private orderByDate(orderByList: OrderBy[] | undefined) {
+    this.articles$.subscribe((articles) => {
+      this.articles$ = this.articles$.pipe(
+        map((articles) => {
+          articles.sort((a, b) => {
+            return a.date > b.date ? 1 : -1;
+          });
+          return articles;
+        }));
+    });
+  }
+
+  private orderByThemes(orderByList: OrderBy[] | undefined): void {
+    this.articles$.subscribe((articles) => {
+      this.articles$ = this.articles$.pipe(
+        map((articles) => {
+          articles.sort((a, b) => {
+            return a.theme > b.theme ? 1 : -1;
+          });
+          return articles;
+        }));
+    });
   }
 }
